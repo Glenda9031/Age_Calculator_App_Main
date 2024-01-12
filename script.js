e941838a1b6820dc3e738f5457e526d5a0870e29
Glenda Joyce Reed <glendareed0807@sbcglobal.net>
2024-01-12 15:28:26 -0500

    Update index.html, style.css and script.js

--- script.js
+++ script.js
@@ -17,10 +17,13 @@ const error_year = document.querySelector('.error-year');
 const error_month = document.querySelector('.error-month');
 const error_day = document.querySelector('.error-day');
 
+submit_btn.addEventListener('click', calculateDate())
+
 input_day.addEventListener('input', (e) => {
     if (+input_day.value > 31) {
         error_day.textContent = 'Must be a valid date';
         isValid = false;
+        return;
     } else {
         isValid = true;
         error_day.textContent = "";
@@ -29,8 +32,64 @@ input_day.addEventListener('input', (e) => {
         isValid = false;
         error_day.textContent = 'This field is required';
         isValid = false;
+        return;
     } else {
         error_day.textContent = "";
     }
 });
 
+input_month.addEventListener('input', (e) => {
+    if (+input_month.value > 12) {
+        error_month.textContent = 'Must be a valid date';
+        isValid = false;
+        return;
+    } else {
+        isValid = true;
+        error_month.textContent = "";
+    }
+    if (+input_month.value === 0) {
+        isValid = false;
+        error_month.textContent = 'This field is required';
+        isValid = false;
+        return;
+    } else {
+        error_month.textContent = "";
+    }
+});
+
+input_year.addEventListener('input', (e) => {
+    if (+input_year.value > 2024) {
+        error_year.textContent = 'Must be a valid date';
+        isValid = false;
+        return;
+    } else {
+        isValid = true;
+        error_year.textContent = "";
+    }
+    if (+input_year.value === 0) {
+        isValid = false;
+        error_year.textContent = 'This field is required';
+        isValid = false;
+        return;
+    } else {
+        error_year.textContent = "";
+    }
+});
+
+function calculateDate() {
+    if (isValid) {
+        let birthday = `${input_month.value}/${input_day.value}/${input_year.value}`;
+        console.log(birthday);
+        let birthdayObj = new Date(birthday);
+        let ageDiffMill = Date.now - birthdayObj;
+        let ageDate = new Date(ageDiffMill);
+        let ageYears = ageDate.getUTCFullYear() - 1970;
+        let ageMonth = ageDate.getUTCMonth();
+        let ageDay = ageDate.getUTCDay();
+        output_day.textContent = ageDay;
+        output_month.textContent = ageMonth;
+        output_year.textContent = ageYears;
+    } else {
+        alert('error');
+    }
+}
