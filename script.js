87e47c72936c4121aba4d7c3c83c4791cf63093a
Glenda Joyce Reed <glendareed0807@sbcglobal.net>
2024-01-11 21:23:33 -0500

    Update index.html, style.css and script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+// Output Elements
+const output_year = document.querySelector('.output-year');
+const output_month = document.querySelector('.output-month');
+const output_day = document.querySelector('.output-day');
+const submit_btn = document.querySelector('.submit-btn');
+
+// Input Elements
+
+let isValid = false;
+const input_year = document.querySelector('#year');
+const input_month = document.querySelector('#month');
+const input_day = document.querySelector('#day');
+
+// Error Elements
+
+const error_year = document.querySelector('.error-year');
+const error_month = document.querySelector('.error-month');
+const error_day = document.querySelector('.error-day');
+
+input_day.addEventListener('input', (e) => {
+    if (+input_day.value > 31) {
+        error_day.textContent = 'Must be a valid date';
+        isValid = false;
+    } else {
+        isValid = true;
+        error_day.textContent = "";
+    }
+    if (+input_day.value === 0) {
+        isValid = false;
+        error_day.textContent = 'This field is required';
+        isValid = false;
+    } else {
+        error_day.textContent = "";
+    }
+});
+
